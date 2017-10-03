efdcd890011f9a74d2dc4b39ffabc90aa04e798f
RCXcrafter <rcxcrafter@gmail.com>
2017-10-03 19:38:49 +0200

    small fix

--- src/main/java/com/rcx/aobdorechunks/proxy/ClientProxy.java
+++ src/main/java/com/rcx/aobdorechunks/proxy/ClientProxy.java
@@ -8,6 +8,6 @@ public class ClientProxy extends CommonProxy {
 
 	@Override
 	public void registerModel(Item item, String stoneType) {
-		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("jaopcaoc:orechunk_" + stoneType, "#inventory"));
+		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation("jaopcaoc:orechunk_" + stoneType + "#inventory"));
 	}
 }
